ba815529c27f42b34b0011d6ad67cd1481d720b3
brad@smithwebtek.com <brad@smithwebtek.com>
2021-07-01 15:53:44 -0400

    Update and style audio markers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 import './App.css';
 import AudioMapPoints from './boundaries/audio_map_points.json';
-import ErrorBoundary from './ErrorBoundary';
+// import ErrorBoundary from './ErrorBoundary';
 import Mic from './assets/mapIcons/mic-fill.svg';
 
 export default function App() {
@@ -15,33 +15,31 @@ export default function App() {
   })
 
   return (
-    <ErrorBoundary>
-      <div className="App">
-        <header className="App-header">
-          <h1>React with MapboxGL</h1>
-          <ReactMapGL
-            {...viewport}
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-            onViewportChange={(viewport) => { setViewport(viewport) }}
-            mapStyle="mapbox://styles/smithwebtek/ckqk5ktyu15no17o62u1pjb8n">
-          </ReactMapGL>
-          <>
-            {AudioMapPoints.features.map(audio => {
-              <Marker
-                key={audio.properties.id}
-                type={audio.type}
-                latitude={audio.geometry.coordinates[1]}
-                longitude={audio.geometry.coordinates[0]}
-                offsetLeft={-20}
-                offsetTop={-10}
-              >
-                <h1>AUDIO</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>React with MapboxGL</h1>
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          onViewportChange={(viewport) => { setViewport(viewport) }}
+          mapStyle="mapbox://styles/smithwebtek/ckqk5ktyu15no17o62u1pjb8n">
+          {AudioMapPoints.features.map(audio => (
+            <Marker
+              key={audio.properties.id}
+              type={audio.type}
+              latitude={audio.geometry.coordinates[1]}
+              longitude={audio.geometry.coordinates[0]}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <button className="marker-btn">
                 <img src={Mic} alt="microphone" />
-              </Marker>
-            })}
-          </>
-        </header>
-      </div>
-    </ErrorBoundary>
+              </button>
+            </Marker>
+          ))
+          })
+        </ReactMapGL>
+      </header>
+    </div>
   );
 }
